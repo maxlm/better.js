b33f5584c0768d8502e9111e1efa57f42fe1e3fe
Max Lozovoy <lozovoy.max@gmail.com>
2013-02-03 17:11:51 +0200

    Declared classes registry added

--- js/better/toolbox/declare.js
+++ js/better/toolbox/declare.js
@@ -19,13 +19,52 @@ define(function(){
     }
 
     /**
+     * Registry of classes declared via 'declare'
+     * @type {Object}
+     */
+    var classRegistry = {};
+
+    function registerClass(/*String*/className, /*Function*/ declaredClass){
+        //summary:
+        //      Register declared class in class registry.
+        //      Private method. Used in 'declare'
+        if(!classRegistry[className]) {
+            classRegistry[className] = declaredClass;
+        } else {
+            var msg = "Attempt to redeclare class." +
+                      "Class '" + className +
+                      "' already declared";
+            throw new Error(msg);
+        }
+    }
+
+    function getDeclaredClass(className) {
+        //summary:
+        //      Get class constructor by class name
+        //return: Constructor|undefined
+        return classRegistry[className];
+    }
+
+    /**
      *  Basic tool for class declaration
      *  Capable to invoke parent class methods via inherited().
      *  Similar to PHP parent::methodName()
      *  That is all for now...
      *  @author Murzik
      */
-    function declare(/* Constructor? */parent,/* Object */props) {
+    function declare(/*String?*/className, /*Constructor?*/parent, /*Object*/props) {
+
+        //<<==params normalization
+        if(typeof className !== "string") {
+            props = parent;
+            parent = className;
+            className = "";
+        }
+        if(!props) {
+            props = parent;
+            parent = false;
+        }
+        //==>>end of params normalization
         var
             hasBase = typeof(parent) == 'function',
             base = hasBase? parent : function() {},
@@ -56,9 +95,14 @@ define(function(){
             classDeclaration.prototype[prop] = propVal;
         }
         classDeclaration.prototype.inherited = inherited;
-
+        if(className) {
+            classDeclaration.prototype.declaredClass = className;
+            registerClass(className, classDeclaration);
+        }
         return classDeclaration;
     }
+
+    declare.getDeclaredClass = getDeclaredClass;
 
     return declare;
 });
