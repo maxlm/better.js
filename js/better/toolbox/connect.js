c9da9100be13f9be5644365b351981b449ebd816
Max Lozovoy <lozovoy.max@gmail.com>
2013-02-09 23:09:50 +0200

    toolbox/connect dependency name refactoring. Some description added

--- js/better/toolbox/connect.js
+++ js/better/toolbox/connect.js
@@ -1,6 +1,14 @@
-define(['jquery', './aop'],function($,aspect){
+define(['jquery', './aspect'],function($,aspect){
     
     function connect(/*Object*/obj,/*String*/methodName,/*Object?*/scope, /*Function*/method){
+        //summary:
+        //      Syntatic sugar for aspect.after
+        //      Connects 'method'(advice) which will be executed in given 'scope'(if provided)
+        //      *after* obj[methodName](target) execution
+        //      See better/toolbox/aspect for more details
+        // returns:
+        //		A signal object that can be used to cancel the advice. If remove() is called on this signal object, it will
+        //		stop the advice function from being executed.
         if ($.isFunction(method)) {
             return aspect.after(obj, methodName, $.proxy(method, scope), true)
         } else {
@@ -11,5 +19,4 @@ define(['jquery', './aop'],function($,aspect){
     }
     
     return connect;
-    
 })
